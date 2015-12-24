2498a83c4cffdec236b91fd1c7033113c31883ac
jiyjb <jiyjb@yonyou.com>
2015-12-24 13:48:56 +0800

    第一个类
    Change-Id: I785ff3e94f5a82db2ed1c3475311d8418bd4bd59
    Signed-off-by: jiyjb <jiyjb@yonyou.com>

---
+++ src/com/test/one/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.test.one;
+/**
+ * HelloWorld
+ * @author Administrator
+ *
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld!");
+	}
+}
